6c5754d2033d7a8a81287222ba33fe098913ff39
yann-ki <yannickkouadio114@gmail.com>
2025-01-10 18:08:03 +0000

    upload sql file

---
+++ sql_load/project_sql/1_top_paying_job.sql
@@ -0,0 +1,24 @@
+--delete this later
+
+
+SELECT
+        job_id,
+        job_title,
+        job_location,
+        job_schedule_type,
+        salary_year_avg,
+        job_posted_date,
+        name as company_name
+
+FROM    job_postings_fact
+
+LEFT JOIN company_dim on job_postings_fact.company_id = company_dim.company_id
+
+WHERE   job_title_short = 'Data Scientist' AND
+        job_location = 'Anywhere' AND
+        salary_year_avg IS NOT NULL
+
+ORDER BY
+        salary_year_avg DESC
+
+LIMIT   10
